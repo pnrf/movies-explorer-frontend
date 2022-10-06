7a5628358b566d6cc71a253523c40b67fb93f3c4
Pankratov <pnrf@yandex.ru>
2022-10-06 04:27:31 +0300

    feat: add adaptive layout for 1280px

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,7 +6,7 @@ function Promo() {
     <section className="promo">
       <img className="promo__bgr-img" src={bgrImg} alt="Фоновое изображение"/>
       <div className="promo__container">
-        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <h1 className="promo__title">Учебный проект студента факультета Веб‑разработки.</h1>
         <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
         <a className="promo__link" href="#about-project">Узнать больше</a>
       </div>
