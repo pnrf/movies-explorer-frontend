e60746a5557c1a79837dcc274b03d1a037c89218
Pankratov <pnrf@yandex.ru>
2025-07-20 17:38:53 +0400

    build: change files

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,18 +9,15 @@ function AboutMe() {
         <img className="about-me__image" src={avatar} alt="Моя фотография" />
         <div className="about-me__info">
           <h3 className="about-me__name">Юрий</h3>
-          <p className="about-me__profession">Юрист, веб-разработчик, 41 год</p>
+          <p className="about-me__profession">Студент</p>
           <p className="about-me__description">
-            20&nbsp;лет отработал юристом в&nbsp;торгово-производственных и&nbsp;консалтинговых компаниях.
-            Оформлял сделки, оптимизировал бизнес-процессы, защищал интересы бизнеса и&nbsp;права граждан в&nbsp;судах и&nbsp;госорганах.
+            После обучения в Яндекс Практикуме собираюсь работать в ИТ.
           </p>
           <p className="about-me__description">
-          В&nbsp;последующие годы планирую развиваться в&nbsp;сфере информационной безопасности.
-          Хочу совместить свои юридические навыки с&nbsp;умением создавать надежные онлайн-сервисы и&nbsp;веб-приложения.
+            Планирую стать техническим писателем. Составлять документацию по программным продуктам и контент для корпоративных баз знаний.
           </p>
           <ul className="about-me__link-list">
-            <li><a className="about-me__link" href="https://github.com/pnrf" target="_blank" rel="noreferrer">Github</a></li>
-            <li><a className="about-me__link" href="https://t.me/pnrf_tg" target="_blank" rel="noreferrer">Telegram</a></li>
+            <li><a className="about-me__link" href="https://github.com/pnrf" target="_blank" rel="noreferrer">Мой Github ↗</a></li>
           </ul>
         </div>
       </div>
